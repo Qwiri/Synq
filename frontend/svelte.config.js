aebbc5994d0b6baa4452f4cc87c0f380435b2076
darmiel <71837281+darmiel@users.noreply.github.com>
2022-05-08 18:29:38 +0200

    fix: svelte kit prerender

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -9,10 +9,11 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-    	pages: 'build',
-    	assets: 'build',
-    	fallback: null
-  })
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
+        prerender: { default: true  }
 	}
 };
 
